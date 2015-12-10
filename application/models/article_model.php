37ca47348887eb197157dfd9bd21fdc2a48731a8
Chintan <chintan@wohlig.com>
2015-12-10 23:26:42 +0530

    some inte

--- application/models/article_model.php
+++ application/models/article_model.php
@@ -1,63 +1,77 @@
 <?php
-if ( !defined( "BASEPATH" ) )
-exit( "No direct script access allowed" );
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
 class article_model extends CI_Model
 {
-public function create($category,$title,$image,$author,$timestamp,$desc)
-{
-$data=array("category" => $category,"title" => $title,"image" => $image,"author" => $author,"timestamp" => $timestamp,"desc" => $desc);
-$query=$this->db->insert( "lll_article", $data );
-$id=$this->db->insert_id();
-if(!$query)
-return  0;
-else
-return  $id;
-}
-public function beforeedit($id)
-{
-$this->db->where("id",$id);
-$query=$this->db->get("lll_article")->row();
-return $query;
-}
-function getsinglearticle($id){
-$this->db->where("id",$id);
-$query=$this->db->get("lll_article")->row();
-return $query;
-}
-public function edit($id,$category,$title,$image,$author,$timestamp,$desc)
-{
-$data=array("category" => $category,"title" => $title,"image" => $image,"author" => $author,"timestamp" => $timestamp,"desc" => $desc);
-$this->db->where( "id", $id );
-$query=$this->db->update( "lll_article", $data );
-return 1;
-}
-public function delete($id)
-{
-$query=$this->db->query("DELETE FROM `lll_article` WHERE `id`='$id'");
-return $query;
-}
-
-public function getcategorydropdown()
-{
-    $query = $this->db->query('SELECT * FROM `lll_category`  ORDER BY `id` ASC')->result();
-    foreach ($query as $row) {
-        $category[$row->id] = $row->title;
+    public function create($category, $title, $image, $author, $timestamp, $desc)
+    {
+        $data = array('category' => $category,'title' => $title,'image' => $image,'author' => $author,'timestamp' => $timestamp,'desc' => $desc);
+        $query = $this->db->insert('lll_article', $data);
+        $id = $this->db->insert_id();
+        if (!$query) {
+            return  0;
+        } else {
+            return  $id;
+        }
     }
-    return $category;
-}
-public function getarticledropdown()
-{
-  $query=$this->db->query("SELECT * FROM `lll_article`  ORDER BY `id` ASC")->result();
-  $return=array(
-  "" => "Select"
+    public function beforeedit($id)
+    {
+        $this->db->where('id', $id);
+        $query = $this->db->get('lll_article')->row();
+
+        return $query;
+    }
+    public function getsinglearticle($id)
+    {
+        $this->db->where('id', $id);
+        $query = $this->db->get('lll_article')->row();
+
+        return $query;
+    }
+    public function edit($id, $category, $title, $image, $author, $timestamp, $desc)
+    {
+        $data = array('category' => $category,'title' => $title,'image' => $image,'author' => $author,'timestamp' => $timestamp,'desc' => $desc);
+        $this->db->where('id', $id);
+        $query = $this->db->update('lll_article', $data);
+
+        return 1;
+    }
+    public function delete($id)
+    {
+        $query = $this->db->query("DELETE FROM `lll_article` WHERE `id`='$id'");
+
+        return $query;
+    }
+
+    public function getcategorydropdown()
+    {
+        $query = $this->db->query('SELECT * FROM `lll_category`  ORDER BY `id` ASC')->result();
+        foreach ($query as $row) {
+            $category[$row->id] = $row->title;
+        }
+
+        return $category;
+    }
+    public function getarticledropdown()
+    {
+        $query = $this->db->query('SELECT * FROM `lll_article`  ORDER BY `id` ASC')->result();
+        $return = array(
+  '' => 'Select',
   );
-  foreach($query as $row)
-  {
-    $return[$row->id]=$row->title;
-  }
+        foreach ($query as $row) {
+            $return[$row->id] = $row->title;
+        }
 
-  return $return;
-}
+        return $return;
+    }
 
+    public function getOne($id)
+    {
+        $query = new stdClass();
+        $query->article = $this->db->query("SELECT * FROM `lll_article` WHERE `id` = '$id'" )->row();
+        $query->recommended = $this->db->query("SELECT * FROM `lll_recommendarticle` INNER JOIN `lll_article` ON `lll_recommendarticle`.`recommendarticle` = `lll_article`.`id`  WHERE `lll_recommendarticle`.`article` = '$id' ORDER BY `lll_article`.`views` DESC  " )->result();
+        return $query;
+    }
 }
-?>
