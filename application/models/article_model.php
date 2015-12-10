e50185310fc3b1ec08bc84c39fae0608b999987b
Chintan <chintan@wohlig.com>
2015-12-10 23:57:25 +0530

    changes for the next and prev

--- application/models/article_model.php
+++ application/models/article_model.php
@@ -70,8 +70,27 @@ class article_model extends CI_Model
     public function getOne($id)
     {
         $query = new stdClass();
+        $this->db->query("UPDATE `lll_article` SET `views`= `views`+1 WHERE `id` = '$id'");
+        $query->category = $this->db->query("SELECT * FROM `lll_article` INNER JOIN `lll_category` ON `lll_category`.`id` =  `lll_article`.`category` WHERE `lll_article`.`id` = '$id'")->row();
         $query->article = $this->db->query("SELECT * FROM `lll_article` WHERE `id` = '$id'" )->row();
         $query->recommended = $this->db->query("SELECT * FROM `lll_recommendarticle` INNER JOIN `lll_article` ON `lll_recommendarticle`.`recommendarticle` = `lll_article`.`id`  WHERE `lll_recommendarticle`.`article` = '$id' ORDER BY `lll_article`.`views` DESC  " )->result();
+
+        $catid = $query->category->id;
+
+        $next = $this->db->query("SELECT `id` FROM `lll_article` WHERE `id` > '$id' AND `category` = '$catid' ORDER BY `id` ASC LIMIT 0,1");
+        if($next->num_rows() > 0)
+        {
+          $query->next = $next->row();
+          $query->next = $query->next->id;
+        }
+
+        $prev= $this->db->query("SELECT `id` FROM `lll_article` WHERE `id` < '$id' AND `category` = '$catid' ORDER BY `id` DESC LIMIT 0,1");
+        if($prev->num_rows() > 0)
+        {
+          $query->prev = $prev->row();
+          $query->prev = $query->prev->id;
+        }
+
         return $query;
     }
 }
